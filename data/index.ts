cc96af8798b2c465cf246d24f533fc2dcae4a576
Manny <ialmosara@gmail.com>
2024-09-08 19:48:23 -0400

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,169 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I prioritize fostering strong relationships with coworkers and open communication, allowing for a strong and productive work environment",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with inter-timezone communication",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Working at Blockgraph",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Want to get in contact with me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sapphire Global Associates",
+    des: "Developed a website for Sapphire Global Associates",
+    img: "",
+    iconLists: ["/re.svg"],
+    link: "https://github.com/ialmosara/SapphireGlobalAssociates",
+  },
+  {
+    id: 2,
+    title: "Flap Flap Revolution",
+    des: "SBU Hacks 2018 Project - Mix of Dance Dance Revolution with Flappy bird (only voice controlled)",
+    img: "",
+    iconLists: [],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Mannys SC2 Bot",
+    des: "Starcraft 2 Bot that can beat Medium level Blizzard bots",
+    img: "",
+    iconLists: [],
+    link: "https://github.com/ialmosara/MannysSC2Bot",
+  },
+];
+
+export const testimonials = [
+  {
+    quote: "Working with Manny",
+    name: "Ravishankar Nagendran",
+    title: "Software Engineering Manager at Blockgraph",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Blockgraph",
+    desc: "Developing a platform that aims to allow marketing companies to secure send targeted ads to ~150-160m households (~300m users) worldwide",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Front end engineer - Stony Brook University",
+    desc: "Spearheaded development revamp of legacy Microscope Simulator tool built by 3 other engineers (371K lines of code) aimed to train ~200 biology students each semester.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ialmosara",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/feed/",
+  },
+];
